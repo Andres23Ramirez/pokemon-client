bd8326a2bd68a10a98f058abe86faf065a50b282
Andres23Ramirez <rodrigo23ramirez@gmail.com>
2022-10-21 09:35:06 -0500

    Feat: create Pokemons context

---
+++ src/contexts/PokemonsContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const context = createContext({});
+ 
+const PokemonsContext = ({children, providedValues}) => {
+   return (
+     <context.Provider value={providedValues}>
+       {children}
+     </context.Provider>
+   );
+}
+
+export { PokemonsContext }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { PokemonsContext } from './contexts/PokemonsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PokemonsContext>
+      <App />
+    </PokemonsContext>
   </React.StrictMode>
 );
 
